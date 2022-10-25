2ed192b6d0f979a5888febc74e69a6ca70248e9a
Horea Oros <horea.oros@gmail.com>
2022-10-25 11:57:46 +0300

    add 25oct project

--- 10_18/10_18/Program.cs
+++ 10_18/10_18/Program.cs
@@ -3,20 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace N1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            P1();
+
+            //P1();
+            //P2();
+            //P3();
+
+            P4();
+
 
         }
 
         /// <summary>
-        /// Calculeaza suma numerelor de la 1 la n
+        /// Determinati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
+        /// </summary>
+        private static void P4()
+        {
+            Console.WriteLine("Introduceti 3 numere naturale - pe o singura linie, separate prin spatiu");
+            int a, b, c;
+
+            string line;
+            line = Console.ReadLine();
+            char[] separator = new char[] { ' ', ',', ';' };
+            string[] tokens = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            a = int.Parse(tokens[0]); 
+            b = int.Parse(tokens[1]); 
+            c = int.Parse(tokens[2]);
+
+            if (a < b + c && b < a + c && c < a + b)
+                Console.WriteLine("Triunghi");
+            else
+                Console.WriteLine("Nu e triunghi");
+            {
+
+            }
+
+        }
+
+        private static void P3()
+        {
+            int n;
+            Console.WriteLine("Introduceti un numar natural!");
+            n = int.Parse(Console.ReadLine());
+            int suma = 0;
+
+            Console.WriteLine($"Suma numerelor de la 1 la {n} este {n * (n + 1)/2}");
+        }
+
+        private static void P2()
+        {
+            int n;
+            Console.WriteLine("Introduceti un numar natural!");
+            n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                suma += i;
+                //Console.WriteLine($"Suma numerelor de la 1 la {i} este {suma}");
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Operatia a durat: {sw.ElapsedMilliseconds}");
+        }
+
+        /// <summary>
+        /// Calculeaza suma numerelor de la 1 la 1, 1 la 2, ...
         /// </summary>
         private static void P1()
         {
---
+++ 10_25/10_25/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //P1();
+            P2();
+        }
+
+        /// <summary>
+        /// Verificam daca un numar este prim
+        /// </summary>
+        private static void P2()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            if (n < 2)
+            {
+                Console.WriteLine("Nu e prim");
+                return;
+            }
+            if (n == 2)
+            {
+                Console.WriteLine("E prim");
+                return;
+            }
+
+            for (int d  = 2; d*d  <= n; d++)
+            {
+                if (n % d == 0)
+                {
+                    Console.WriteLine("Nu e prim");
+                    return;
+                }
+            }
+            Console.WriteLine("E prim");
+
+
+        }
+
+        /// <summary>
+        /// Se citesc 3 numere de pe o linie a ecranului 
+        /// si trebuie afisate in ordine crescatoare
+        /// </summary>
+        /// <example>Daca se citeste 5 3 2 se afiseaza 2 3 5</example>
+        /// <exception cref="OverflowException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        private static void P1()
+        {
+            int a, b, c;
+            string line;
+
+            line = Console.ReadLine();
+            char[] sep = { ' ', ';', ','  };
+            string[] tokens = line.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+            a = int.Parse(tokens[0]); 
+            b = int.Parse(tokens[1]); 
+            c = int.Parse(tokens[2]);
+
+            // Varianta 1;
+
+            int aux;
+            if (a > b)
+            {
+                aux = a;
+                a = b;
+                b = aux;
+
+                // alternativa
+                
+                //a = a + b;
+                //b = a - b;
+                //a = a - b;
+                //a   b
+                //-----
+                //1   2
+                //3
+                //    1
+                //2
+
+            }
+            if (a > c)
+            {
+                aux = a;
+                a = c;
+                c = aux;
+            }
+            if (b > c)
+            {
+                aux = b;
+                b = c;
+                c = aux;
+            }
+
+            Console.WriteLine($"{a} {b} {c}");
+
+            // Varianta 2
+            int minim;
+            minim = a;
+            if (b < minim)
+            {
+                minim = b;
+            }
+            if (c < minim)
+            {
+                minim = c;
+            }
+            int maxim;
+            maxim = a;
+            if (b > maxim)
+            {
+                maxim = b;
+            }
+            if (c > maxim)
+            {
+                maxim = c;
+            }
+
+            Console.WriteLine($"{minim} {a + b + c - minim - maxim} {maxim}");
+
+            // Varianta 3
+            minim = Math.Min(a, Math.Min(b, c));
+            maxim = Math.Max(b, Math.Max(c, a));
+            Console.WriteLine($"{minim} {a + b + c - minim - maxim} {maxim}");
+        }
+    }
+}
