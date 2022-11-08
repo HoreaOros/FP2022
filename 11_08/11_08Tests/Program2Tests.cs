9f855b465f858810ecbf3be6d4073bd5df9a2384
Horea Oros <horea.oros@gmail.com>
2022-11-08 11:55:17 +0200

    add 11_08

---
+++ 11_08/11_08Tests/Program2Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _11_08;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_08.Tests
+{
+    [TestClass()]
+    public class Program2Tests
+    {
+        [TestMethod()]
+        public void NumarCifreTest()
+        {
+            // arrange
+            int n = -25487;
+            int expected = 5;
+
+            // act 
+            int actual = Program2.NumarCifre(n);
+            
+            // assert
+            Assert.IsTrue(actual == expected);
+            
+        }
+    }
+}
