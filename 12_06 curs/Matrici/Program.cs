6bfef0aa44daa404dc717f66fd85114fdbb0be42
Horea Oros <horea.oros@gmail.com>
2022-12-06 11:56:46 +0200

    add 12_06 projects, QuickSort, Matrix multiplication

---
+++ 12_06 curs/Matrici/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrici
+{
+    internal class Program
+    {
+        private static readonly Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matrici");
+            int[,] data1 = InitMatrix(3, 4, 100);
+            PrintMatrix(data1, "data1");
+
+            int[,] data2 = InitMatrix(4, 5, 100);
+            PrintMatrix(data2, "data2");
+            
+            int[,] data3 = Multiply(data1, data2);
+            PrintMatrix(data3, "data1*data2");
+        }
+
+        private static int[,] Multiply(int[,] data1, int[,] data2)
+        {
+            if (data1.GetLength(1) != data2.GetLength(0))
+            {
+                throw new ArgumentException();
+            }
+            int[,] result = new int[data1.GetLength(0), data2.GetLength(1)];
+            for (int i = 0; i < data1.GetLength(0); i++)
+            {
+                for (int j = 0; j < data2.GetLength(1); j++)
+                {
+                    int suma = 0;
+                    for (int k = 0; k < data1.GetLength(1); k++)
+                    {
+                        suma += data1[i, k] * data2[k, j];
+                    }
+                    result[i, j] = suma;
+                }
+            }
+            return result;
+        }
+
+        private static void PrintMatrix(int[,] data, string mesaj)
+        {
+            Console.WriteLine(mesaj);
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write($"{data[i, j], 3} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static int[,] InitMatrix(int linii, int coloane, int maxim)
+        {
+            int[,] data = new int[linii, coloane];
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    data[i, j] = rnd.Next(maxim);
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ 12_06/12_06/MinMaxK.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_06
+{
+    internal class MinMaxK
+    {
+        static void Main(string[] args)
+        {
+            int k;
+
+            do
+            {
+                k = ExtractNextInt();
+                Console.WriteLine(k);
+            } while (k != 0);
+            
+
+
+        }
+
+        private static int ExtractNextInt()
+        {
+            int nr = 0;
+            int ch;
+            do
+            {
+                ch = Console.Read();
+                if (ch == -1)
+                {
+                    return 0;
+                }
+            } while (char.IsWhiteSpace((char)ch));
+            
+            while (char.IsDigit((char)ch))
+            {
+                nr = nr * 10 + ch - '0';
+                ch = Console.Read();
+                if (ch == -1)
+                {
+                    return 0;
+                }
+            }
+
+            
+
+            return nr;
+        }
+    }
+}
