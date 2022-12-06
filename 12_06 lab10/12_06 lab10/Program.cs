aa46f70b39871e950c1b051a8ec533edf45dbeea
Horea Oros <horea.oros@gmail.com>
2022-12-06 12:18:22 +0200

    add laborator 10

---
+++ 12_06 lab10/12_06 lab10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_06_lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
